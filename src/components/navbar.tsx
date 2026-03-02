8ad8e0549d6ce2a688272476a8751a5b053099a9
Michael Savage <michaelsavage940@gmail.com>
2026-03-03 00:18:03 +0100

    add Hanneat font

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,15 +11,15 @@ const links = [
 export const Navbar = () => {
   return (
     <header className="fixed top-4 left-1/2 transform -translate-x-1/2 z-60 transition-all">
-      <nav className="bg-secondary rounded-2xl flex items-center justify-between gap-16 px-8 py-4 transition-all duration-200 w-full">
-        <h1 className="text-xl font-semibold text-white hover:text-silver text-nowrap">
+      <nav className="text-dark-blue rounded-2xl flex items-center justify-between gap-16 px-8 py-4 transition-all duration-200 w-full">
+        <h1 className="text-xl font-bold text-nowrap hover:text-primary-blue uppercase">
           <Link to="/">{SITE_NAME}</Link>
         </h1>
 
         <ul className="flex items-center gap-6">
           {links.map((link) => (
-            <li key={link.to} className="text-lg">
-              <Link to={link.to} className="text-white hover:text-silver">
+            <li key={link.to} className="text-lg uppercase font-semibold">
+              <Link to={link.to} className="hover:text-primary-blue">
                 {link.label}
               </Link>
             </li>
